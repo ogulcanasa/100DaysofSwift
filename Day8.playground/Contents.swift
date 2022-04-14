8831851097fba1e00fc41146dc2e3c0f31bddbff
Ogulcan Asa <ogulcanasa@hotmail.com>
2022-04-14 22:51:01 +0300

    Day8 is completed.

---
+++ Day8.playground/Contents.swift
@@ -0,0 +1,90 @@
+import UIKit
+
+// 1.Creating your own structs
+
+struct Sport {
+    var name: String
+}
+
+var tennis = Sport(name: "Tennis")
+print(tennis.name)
+tennis.name = "Lawn tennis"
+
+// 2.Computed properties
+
+struct Sport2 {
+    var name: String
+    var isOlympicSport: Bool
+    
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+let chessBoxing = Sport2(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+// 3.Property observers (didSet -> This will run some code every time amount changes)
+
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+// 4.Methods
+
+struct City {
+    var population: Int
+    
+    func collectTaxes() -> Int {
+        return population * 200
+    }
+}
+
+let london = City(population: 9_000_000)
+london.collectTaxes()
+
+// 5.Mutating methods
+
+struct Person {
+    var name: String
+    
+    mutating func makeAnonymous() {
+        name = "Anonymous"
+    }
+}
+
+var person = Person(name: "Ed")
+person.makeAnonymous()
+person.name
+
+// 6.Properties and methods of strings
+
+let string = "Do or do not, there is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
+// 7.Properties and methods of arrays
+
+var toys = ["Woody","Fizz"]
+print(toys.count)
+toys.append("Buzz")
+toys.firstIndex(of: "Buzz")
+print(toys.sorted())
+toys.remove(at: 0)
+
